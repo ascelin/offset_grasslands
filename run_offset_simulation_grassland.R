5ebb8782efccf91e16138ec8b7807f1ade7e3fe5
isaacpeterson <isaac.p.mail@gmail.com>
2018-04-24 10:44:55 +1000

    remove hard coded run folder address

--- run_offset_simulation_grassland.R
+++ run_offset_simulation_grassland.R
@@ -7,5 +7,5 @@ user_global_params = initialise_user_global_params()
 user_output_params <- initialise_user_output_params()
 
 osim.run(user_global_params, user_simulation_params, loglevel = 'TRACE')
-current_simulation_folder = find_current_run_folder(run_number = 10)
+current_simulation_folder = find_current_run_folder()
 osim.output(user_output_params, current_simulation_folder, loglevel = 'TRACE')
