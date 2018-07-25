97ada81706ddca4067c7f066823aab3d2318bf5a
isaacpeterson <isaac.p.mail@gmail.com>
2018-07-25 12:02:43 +1000

    update params etc to dynamics code inputs

--- load_grassland_data.R
+++ load_grassland_data.R
@@ -17,12 +17,9 @@ find_disjoint_probabilty_list <- function(pool_mask, land_parcels){
   return(probability_list)
 }
 
-data_type = 'grassland'
 offset_into_disjoint_region = TRUE
 
 max_eco_val = 100
-mean_decline_rate = -0.02
-decline_rate_std = 0.005
 
 save_site_planning_units = FALSE
 save_ecology = TRUE
@@ -34,42 +31,27 @@ simulation_inputs_folder = ('simulation_inputs/')
 objects_to_save = list()
 
 planning_units_array = raster_to_array(raster('data/planning.units.uid_20ha.asc'))
-parcels <- define_planning_units(planning_units_array)
+site_characteristics <- define_planning_units(planning_units_array)
 
 if (save_site_planning_units == TRUE){
   objects_to_save$planning_units_array <- planning_units_array
-  objects_to_save$parcels <- parcels
-}
-
-if (save_ecology == TRUE){
-  landscape_ecology <- list(raster_to_array(raster('data/hab.map.master.zo1.asc')))
-  landscape_ecology <- scale_ecology(landscape_ecology, max_eco_val, dim(landscape_ecology[[1]]))
-  objects_to_save$landscape_ecology <- landscape_ecology
-  objects_to_save$parcel_ecology <- split_ecology(landscape_ecology, parcels$land_parcels)
-}
-
-if (save_offset_region == TRUE){
-  if (offset_into_disjoint_region == TRUE){
-    dev_pool_mask = raster('data/dev_pool_mask.asc')
-    dev_probabilty_list = find_disjoint_probabilty_list(pool_mask = dev_pool_mask, land_parcels = parcels$land_parcels)
-    offset_pool_mask = raster('data/offset_pool_mask_random.asc')
-    offset_probabilty_list = find_disjoint_probabilty_list(pool_mask = offset_pool_mask, land_parcels = parcels$land_parcels)
-  } else {
-    dev_probabilty_list = rep(list(1/length(parcels$land_parcels)), length(parcels$land_parcels))
-    offset_probabilty_list = dev_probabilty_list
-  }
-  
-  dev_probabilty_list[[4]] = 0 #set probability of particular site to zero to exclude from offset group
-  offset_probabilty_list[[4]] = 0 #set probability of particular site to zero to exclude from development group
-  objects_to_save$dev_probabilty_list <- dev_probabilty_list
-  objects_to_save$offset_probabilty_list <- offset_probabilty_list
+  objects_to_save$site_characteristics <- site_characteristics
 }
 
 
-if (save_decline_rates == TRUE){
-  objects_to_save$decline_rates_initial = simulate_decline_rates(parcel_num = length(parcels$land_parcels), 
-                                                       sample_decline_rate = TRUE, 
-                                                       mean_decline_rate, 
-                                                       decline_rate_std)       # set up array of decline rates that are eassociated with each cell
+if (offset_into_disjoint_region == TRUE){
+  dev_pool_mask = raster('data/dev_pool_mask.asc')
+  dev_probabilty_list = find_disjoint_probabilty_list(pool_mask = dev_pool_mask, land_parcels = site_characteristics$land_parcels)
+  offset_pool_mask = raster('data/offset_pool_mask_random.asc')
+  offset_probabilty_list = find_disjoint_probabilty_list(pool_mask = offset_pool_mask, land_parcels = site_characteristics$land_parcels)
+} else {
+  dev_probabilty_list = rep(list(1/length(site_characteristics$land_parcels)), length(site_characteristics$land_parcels))
+  offset_probabilty_list = dev_probabilty_list
 }
+
+dev_probabilty_list[[4]] = 0 #set probability of particular site to zero to exclude from offset group
+offset_probabilty_list[[4]] = 0 #set probability of particular site to zero to exclude from development group
+objects_to_save$dev_probabilty_list <- dev_probabilty_list
+objects_to_save$offset_probabilty_list <- offset_probabilty_list
+
 save_simulation_inputs(objects_to_save, simulation_inputs_folder)
