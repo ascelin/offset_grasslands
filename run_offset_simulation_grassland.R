fe4a943646f0fd58846d1313bb8b73a25205df1e
isaacpeterson <isaac.p.mail@gmail.com>
2018-08-28 10:34:13 +1000

    update params to match repo

--- run_offset_simulation_grassland.R
+++ run_offset_simulation_grassland.R
@@ -1,11 +1,11 @@
 library(offsetsim)
-source('test_grassland_params.R' )
+source('grassland_params.R' )
 
 user_simulation_params = initialise_user_simulation_params()
 user_global_params = initialise_user_global_params()
 user_feature_params = initialise_user_feature_params()
 user_output_params <- initialise_user_output_params()
 
-osim.run(user_global_params, user_simulation_params, user_feature_params, loglevel = 'TRACE')
+#osim.run(user_global_params, user_simulation_params, user_feature_params, loglevel = 'TRACE')
 current_simulation_folder = find_current_run_folder(base_folder = '~/offset_data/grassland/')
 osim.output(user_output_params, current_simulation_folder, loglevel = 'TRACE')
